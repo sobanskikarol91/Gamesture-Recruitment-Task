a605fc8a7d643a58b363dc3b1633ea6bf56f4c44
Karol Sobański <sobanskikarol91@gmail.com>
2020-01-16 22:20:27 +0100

    Add: functionality to all buttons

---
+++ Gamesture/Assets/Scripts/DisplayObjectForAMoment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class DisplayObjectForAMoment : MonoBehaviour
+{
+    [SerializeField] float displayTime;
+    [SerializeField] GameObject objectToDisplay;
+
+    private bool isRunning;
+    private Coroutine coroutine;
+
+    public void Display()
+    {
+        if (isRunning)
+            StopCoroutine(coroutine);
+
+        coroutine = StartCoroutine(IEDisplay());
+    }
+
+    IEnumerator IEDisplay()
+    {
+        isRunning = true;
+        objectToDisplay.SetActive(true);
+        yield return new WaitForSeconds(displayTime);
+        objectToDisplay.SetActive(false);
+        isRunning = false;
+    }
+
+    private void OnDisable()
+    {
+        objectToDisplay.SetActive(false);
+    }
+}
